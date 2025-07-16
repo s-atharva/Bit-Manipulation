91c6d983799b311347d03f2ab9c33dd6bb2782a8
s-atharva <atharvasarbhukan@gmail.com>
2025-07-16 22:16:37 +0530

    check ith beat is set or not and clean beat set

--- check_ith_beat_set_or_not.py
+++ check_ith_beat_set_or_not.py
@@ -1,10 +1,16 @@
-def is_ith_beat_set(Number, ith_beat_set):
-    if Number & (1 << ith_beat_set):
+def is_ith_beat_set(number, ith_beat_set):
+    if number & (1 << ith_beat_set):
         print("True")
     else:
         print("False")
 
 
+def clear_ith_beat_set(number, ith_beat_set):
+    number = number & ~(1 << ith_beat_set)
+    print(number)
+
+
 N = 13
 i = 2
 is_ith_beat_set(N, i)
+clear_ith_beat_set(N, i)
